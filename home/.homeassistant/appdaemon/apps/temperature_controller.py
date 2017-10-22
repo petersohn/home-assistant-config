d457c52299a3c38e3a9cc885ad284a44620c2b63
Péter Szabados <kangirigungi@gmail.com>
2017-10-22 08:01:27 +0000

    Improve temperature controller algorithm

--- home/.homeassistant/appdaemon/apps/temperature_controller.py
+++ home/.homeassistant/appdaemon/apps/temperature_controller.py
@@ -5,9 +5,15 @@ class TemperatureController(appapi.AppDaemon):
 
     def initialize(self):
         self.__target = float(self.args['target'])
+        self.__minimum = float(self.args['minimum'])
         self.__tolerance = float(self.args.get('tolerance', 1.0))
+        self.__low_mode_start = float(
+            self.args.get('low_mode_start', 30)) * 60
+        self.__low_mode_time = float(self.args.get('low_mode_time', 5)) * 60
+        self.__timer = None
         self.listen_state(self.trigger, entity=self.args['sensor'])
         self.listen_state(self.trigger, entity=self.args['availability'])
+
         self.__check()
 
     def trigger(self, entity, attribute, old, new, kwargs):
@@ -16,7 +22,40 @@ class TemperatureController(appapi.AppDaemon):
     def __check(self):
         temperature = float(self.get_state(self.args['sensor']))
 
-        if temperature < self.__target - self.__tolerance:
-            self.turn_off(self.args['switch'])
-        elif temperature > self.__target + self.__tolerance:
+        if temperature > self.__target + self.__tolerance:
+            self.__stop_timer()
             self.turn_on(self.args['switch'])
+        elif temperature < self.__minimum:
+            self.__stop_timer()
+            self.turn_off(self.args['switch'])
+        elif temperature < self.__target - self.__tolerance:
+            if self.__timer is None:
+                self.__schedule_start()
+            self.turn_off(self.args['switch'])
+
+    def __schedule_start(self):
+        self.__stop_timer()
+        self.log('Schedule low mode start')
+        self.__timer = self.run_in(
+            self.__on_start_timer, self.__low_mode_start)
+
+    def __schedule_stop(self):
+        self.__stop_timer()
+        self.log('Schedule low mode stop')
+        self.__timer = self.run_in(self.__on_stop_timer, self.__low_mode_time)
+
+    def __stop_timer(self):
+        if self.__timer is not None:
+            self.log('Stop timer')
+            self.cancel_timer(self.__timer)
+            self.__timer = None
+
+    def __on_start_timer(self, kwargs):
+        self.log('Low mode start')
+        self.turn_on(self.args['switch'])
+        self.__schedule_stop()
+
+    def __on_stop_timer(self, kwargs):
+        self.log('Low mode stop')
+        self.turn_off(self.args['switch'])
+        self.__schedule_start()
