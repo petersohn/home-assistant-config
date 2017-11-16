e9a6bdd727d3d54e7b52b77860f168331aa4af33
Péter Szabados <kangirigungi@gmail.com>
2017-11-16 15:33:15 +0000

    Add missing measurements

--- home/.homeassistant/appdaemon/apps/motion_sensor.py
+++ home/.homeassistant/appdaemon/apps/motion_sensor.py
@@ -16,14 +16,17 @@ class MotionSensor(appapi.AppDaemon):
 
     def __on_motion_start(self, entity, attribute, old, new, kwargs):
         self.__stop_timer()
+        self.log('Switching on')
         for target in self.__targets:
             self.turn_on(target)
 
     def __on_motion_stop(self, entity, attribute, old, new, kwargs):
         if all([self.get_state(sensor) == 'off' for sensor in self.__sensors]):
+            self.log('Starting timer')
             self.__timer = self.run_in(self.__on_timeout, self.__time)
 
     def __on_timeout(self, kwargs):
+        self.log('Switching off')
         for target in self.__targets:
             self.turn_off(target)
 
