b270074d8e3a5d7a78b366aa2714dce1fab4b10d
Peter Szabados <kangirigungi@gmail.com>
2019-07-28 21:05:19 +0200

    Keep trying to switch auto switch

--- home/.homeassistant/appdaemon/apps/auto_switch.py
+++ home/.homeassistant/appdaemon/apps/auto_switch.py
@@ -7,6 +7,7 @@ class AutoSwitch(hass.Hass):
         self.__switch = self.args.get('switch')
         self.__reentrant = self.args.get('reentrant', False)
         self.__intended_state = None
+        self.__timer = None
 
         self.listen_state(self.__on_target_change, entity=self.__target)
 
@@ -49,6 +50,7 @@ class AutoSwitch(hass.Hass):
             self.__update(0)
 
     def __update(self, state):
+        self.__stop_timer()
         self.log('Got new state: ' + str(state))
         self.__state = state
 
@@ -67,14 +69,17 @@ class AutoSwitch(hass.Hass):
         self.log('Turning ' + state)
         if self.get_state(self.__target) != state:
             self.__intended_state = state
+            self.__timer = self.run_in(lambda _: self.__update(), 10)
 
     def __on_switch_change(self, entity, attribute, old, new, kwargs):
         if new == 'on':
             self.log('Manually turning on')
             self.turn_on(self.__target)
+            self.__stop_timer()
         elif new == 'off':
             self.log('Manually turning off')
             self.turn_off(self.__target)
+            self.__stop_timer()
         else:
             self.log('Setting to auto')
             self.__update(self.__state)
@@ -84,8 +89,15 @@ class AutoSwitch(hass.Hass):
             self.log('Switching to manual mode')
             self.select_option(entity_id=self.__switch, option=new)
             self.__intended_state = None
+            self.__stop_timer()
         elif new == self.__intended_state:
             self.__intended_state = None
+            self.__stop_timer()
+
+    def __stop_timer(self):
+        if self.__timer:
+            self.cancel_timer(self.__timer)
+            self.__timer = None
 
 
 class Switcher:
