19b2ae8fa330b11e217b7f8b8521b899bac48165
Péter Szabados <pszabados@protonmail.com>
2021-10-23 18:54:45 +0200

    Fix mutex

--- home/.homeassistant/appdaemon/apps/alert.py
+++ home/.homeassistant/appdaemon/apps/alert.py
@@ -9,39 +9,41 @@ class AlertAggregator(hass.Hass):
             self.entity = entity
             extra_values = {'name': entity}
             self.trigger_expr = expression.ExpressionEvaluator(
-                app, trigger_expr, self._on_change, extra_values)
+                app, trigger_expr, self.on_change, extra_values)
             self.text_expr = expression.ExpressionEvaluator(
-                app, text_expr, None, extra_values)
+                app, text_expr, self.on_text_changed, extra_values)
             self.value = self._calculate_trigger_value()
             self.text_value = self.text_expr.get()
             self.timer = None
 
-        def _on_text_changed(self, value):
-            self.text_value = value
+        def on_text_changed(self, value):
+            with self.mutex.lock('on_text_changed'):
+                self.text_value = value
 
-        def _on_change(self, value):
-            if value:
-                if self.app.timeout is not None:
-                    if self.value:
-                        self.app.error('Value is already set: {}'.format(
-                            self.entity))
+        def on_change(self, value):
+            with self.mutex.lock('on_change'):
+                if value:
+                    if self.app.timeout is not None:
+                        if self.value:
+                            self.app.error('Value is already set: {}'.format(
+                                self.entity))
+                            return
+
+                        if self.timer is None:
+                            self.timer = self.app.run_in(
+                                lambda kwargs: self._handle_change(value),
+                                self.app.timeout.total_seconds())
+                        else:
+                            self.app.error('Timer is already set: {}'.format(
+                                self.entity))
                         return
+                else:
+                    if self.timer is not None:
+                        self.app.cancel_timer(self.timer)
+                        self.timer = None
 
-                    if self.timer is None:
-                        self.timer = self.app.run_in(
-                            lambda kwargs: self._handle_change(value),
-                            self.app.timeout.total_seconds())
-                    else:
-                        self.app.error('Timer is already set: {}'.format(
-                            self.entity))
-                    return
-            else:
-                if self.timer is not None:
-                    self.app.cancel_timer(self.timer)
-                    self.timer = None
-
-            if value != self.value:
-                self._handle_change(value)
+                if value != self.value:
+                    self._handle_change(value)
 
         def _handle_change(self, value):
             self.value = value
@@ -80,6 +82,9 @@ class AlertAggregator(hass.Hass):
             self.log('Alert is initially on')
             self._turn_on()
 
+        for source in self.sources:
+            self.log('Source {} -> {}'.format(source.entity, source.get_text_value()))
+
         self.mutex = self.get_app('locker').get_mutex('AlertAggregator')
 
     def terminate(self):
@@ -97,17 +102,16 @@ class AlertAggregator(hass.Hass):
                 if source.get_trigger_value())})
 
     def on_change(self, entity, value):
-        with self.mutex.lock('on_change'):
-            if value:
-                self.log('Alert turned on for {}'.format(entity))
-                self._turn_off()
-                self._turn_on()
-                return
-
-            if not any(source.get_trigger_value() for source in self.sources):
-                self.log('Resetting alert')
-                self._turn_off()
-                return
-
-            self.log('Setting alert')
+        if value:
+            self.log('Alert turned on for {}'.format(entity))
+            self._turn_off()
             self._turn_on()
+            return
+
+        if not any(source.get_trigger_value() for source in self.sources):
+            self.log('Resetting alert')
+            self._turn_off()
+            return
+
+        self.log('Setting alert')
+        self._turn_on()
