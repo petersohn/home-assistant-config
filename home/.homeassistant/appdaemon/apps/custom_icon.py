caef2f7ef3302474a78f18b06f8ec62ef0dc7a15
Péter Szabados <pszabados@protonmail.com>
2025-01-30 19:03:14 +0100

    More camera stuff

--- home/.homeassistant/appdaemon/apps/custom_icon.py
+++ home/.homeassistant/appdaemon/apps/custom_icon.py
@@ -8,13 +8,16 @@ class CustomIcon(hass.Hass):
 
         self.mutex = self.get_app('locker').get_mutex('CustomIcon')
 
+        self.log('asdasd')
         for entity in self.args['entities']:
             self.listen_state(self.on_changed, entity=entity)
 
     def on_changed(self, entity, attribute, old, new, kwargs):
-        with self.mutex.lock('on_changed'):
-            state = self.get_state(entity=entity, attribute='all')
-            icon = self.on_icon if state['state'] == 'on' else self.off_icon
-            state['attributes']['icon'] = icon
-            self.log("set state: {}".format(state['attributes']))
-            self.set_state(entity, attributes=state['attributes'])
+        pass
+        #self.log('lofasz')
+        #with self.mutex.lock('on_changed'):
+        #    state = self.get_state(entity=entity, attribute='all')
+        #    icon = self.on_icon if state['state'] == 'on' else self.off_icon
+        #    state['attributes']['icon'] = icon
+        #    self.log("set state: {}".format(state['attributes']))
+        #    self.set_state(entity, attributes=state['attributes'])
