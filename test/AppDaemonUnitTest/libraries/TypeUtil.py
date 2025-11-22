07d7031907b116cdbb27d4cc2a8e33f41fa015a4
Peter Szabados <kangirigungi@gmail.com>
2025-11-22 19:20:53 +0100

    wtf

--- test/AppDaemonUnitTest/libraries/TypeUtil.py
+++ test/AppDaemonUnitTest/libraries/TypeUtil.py
@@ -2,6 +2,12 @@ from typing import Any
 
 
 def extract_from_dictionary(dictionary: Any, key: Any) -> Any:
+    return _extract_from_dictionary(dictionary, key)
+
+
+def _extract_from_dictionary[Key, Value](
+    dictionary: dict[Key, Value], key: Key
+) -> Value | None:
     result = None
     if key in dictionary:
         result = dictionary[key]
