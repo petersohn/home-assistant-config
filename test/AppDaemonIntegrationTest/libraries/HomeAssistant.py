93cea098b45eb5516c5c9396f4d4841b2ccfd492
Peter Szabados <kangirigungi@gmail.com>
2025-12-14 14:00:21 +0100

    Remove unused libraries

--- test/AppDaemonIntegrationTest/libraries/HomeAssistant.py
+++ test/AppDaemonIntegrationTest/libraries/HomeAssistant.py
@@ -6,23 +6,12 @@ import Directories
 
 def create_home_assistant_configuration(target_directory, port):
     source_file = os.path.join(
-        Directories.hass_config_path, 'configuration.yaml')
+        Directories.hass_config_path, "configuration.yaml"
+    )
     with open(source_file) as source:
         content = yaml.safe_load(source)
-    content['http']['server_port'] = port
+    content["http"]["server_port"] = port
     os.makedirs(target_directory)
-    target_file = os.path.join(target_directory, 'configuration.yaml')
-    with open(target_file, 'w') as target:
+    target_file = os.path.join(target_directory, "configuration.yaml")
+    with open(target_file, "w") as target:
         yaml.dump(content, target)
-#
-#
-# def create_home_assistant_auth(target_directory, port):
-#     source_file = os.path.join(
-#         Directories.hass_config_path, 'auth')
-#     with open(source_file) as source:
-#         content = json.load(source)
-#     content['http']['server_port'] = port
-#     os.makedirs(target_directory)
-#     target_file = os.path.join(target_directory, 'configuration.yaml')
-#     with open(target_file, 'w') as target:
-#         yaml.dump(content, target)
