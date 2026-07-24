e7867b8cc2eb2a6100fcf86751dfe1b8525cdb3e
Péter Szabados <pszabados@protonmail.com>
2026-07-24 20:35:24 +0200

    more ci fix

--- test/appdaemon_integration_test/helpers/directories.py
+++ test/appdaemon_integration_test/helpers/directories.py
@@ -1,6 +1,6 @@
 import os
 
-base_path = os.environ["PWD"]
+base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 config_path = os.path.join(base_path, "config")
 hass_config_path = os.path.join(config_path, "hass")
 appdaemon_config_path = os.path.join(config_path, "appdaemon")
